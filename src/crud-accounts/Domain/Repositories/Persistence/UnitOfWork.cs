2393fa8ace4021419c82d6c19a0668bbf08756cc
Cleiton dos Santos Cordeiro <cleitonsancor@hotmail.com>
2021-02-24 14:03:33 -0300

    Implementando UnitOfWork.

---
+++ src/crud-accounts/Domain/Repositories/Persistence/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using crud_accounts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crudAccounts.Domain.Repositories.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApiDbContext _context;
+
+        public UnitOfWork(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
