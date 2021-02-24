a94596511ebdb8ebecac83f5b9e38dc9a3bf3ddb
Cleiton dos Santos Cordeiro <cleitonsancor@hotmail.com>
2021-02-23 21:06:26 -0300

    Configurando Local DB.

---
+++ src/crud-accounts/Models/ApiDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud_accounts.Models
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Telefone> Telefones { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApiDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
